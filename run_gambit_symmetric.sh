d6c740eceb89017d0312d7ec52d1ed70e90e55d4
accxev <antonia.schmalstieg@googlemail.com>
2017-06-19 19:35:43 +0200

    Script now has the game type as parameter

--- run_gambit_symmetric.sh
+++ run_gambit_symmetric.sh
@@ -4,20 +4,22 @@
 
 no_actions=$1
 iterations=$2
-saddle_type=$3
+game_type=$3
 
 cd GAMUT
 
-mkdir -p games/$no_actions
+mkdir -p games/$game_type/$no_actions
+mkdir -p games/$game_type/counters
 
 cd gamut
 
 
 timestamp=$(date +"%Y-%m-%d_%H-%M-%S")
-#for i in 1 2 3 4 5; do
+
 for i in $(seq $iterations); do
 	# change this line if you want to generate other game classes
-	java edu.stanford.multiagent.gamer.Main -g SymmetricRandomZeroSum -actions $no_actions -f ../games/$no_actions/SymmetricRandomZeroSum_${no_actions}_${timestamp}_$i.game -output GambitOutput
+	java edu.stanford.multiagent.gamer.Main -g $game_type -actions $no_actions -f ../games/$game_type/$no_actions/${game_type}_${no_actions}_${timestamp}_$i.game -output GambitOutput
+	#java edu.stanford.multiagent.gamer.Main -g SymmetricRandomZeroSum -actions $no_actions -f ../games/$game_type/$no_actions/SymmetricRandomZeroSum_${no_actions}_${timestamp}_$i.game -output GambitOutput
 	#java edu.stanford.multiagent.gamer.Main -g SymmetricRandomZeroSum -actions $no_actions -f ../games/SymmetricRandomZeroSum_${timestamp}_$i.game -output GambitOutput
 	#java -classpath build/classes/main edu.stanford.multiagent.gamer.Main "$@" -g SymmetricRandomZeroSum -actions 4 -f ../games/SymmetricRandomZeroSum_${timestamp}_$i.game -output GambitOutput
 	# java -classpath /GAMUT/gamut/build/classes/main edu.stanford.multiagent.gamer.Main "$@" -g SymmetricRandomZeroSum -actions 2
@@ -30,7 +32,7 @@ cd ..
 
 # iterate over all game files in current directory
 #for f in GAMUT/games/*/*.game; do
-for f in GAMUT/games/*/*.game; do
+for f in GAMUT/games/$game_type/$no_actions/*.game; do
 
 	# this block computes nash equilibria for the generated games. Uncomment if you want and/or need that.
 
@@ -39,6 +41,9 @@ for f in GAMUT/games/*/*.game; do
     #	gambit-enumpoly $f -q | tee ${f%.game}.nash #$f.nash	# executing gambit with command line output of the NE
     #	#gambit-enumpoly $f -q > ${f%.game}.nash #$f.nash 		# executing gambit without command line output of the NE
 	#fi
+
+
+	# TODO probably obsolete
 	#if [ ! -f ${f%.game}.saddle ]; then
 	#	echo "Running SaddlePy on file $f"
 	#	python ../SaddlePy/main.py $f
@@ -49,18 +54,18 @@ for f in GAMUT/games/*/*.game; do
 	if [ "$saddle_type"=="s" ]; then
 		if [ ! -f ${f%.game}.saddle ]; then
 			echo "Running SaddlePy for strict saddles on file $f"
-			python ../SaddlePy/main.py $f $saddle_type
+			python ../SaddlePy/main.py $f GAMUT/games/$game_type #"s" #$saddle_type
 		fi
 	fi
 
 	# TODO weak saddles
 
-	if [ "$saddle_type"=="v" ]; then
-		if [ ! -f ${f%.game}.vsaddle ]; then
-			echo "Running SaddlePy for very weak saddles on file $f"
-			python ../SaddlePy/main.py $f $saddle_type
-		fi
-	fi
+	#if [ "$saddle_type"=="v" ]; then
+	#	if [ ! -f ${f%.game}.vsaddle ]; then
+	#		echo "Running SaddlePy for very weak saddles on file $f"
+	#		python ../SaddlePy/main.py $f $saddle_type
+	#	fi
+	#fi
 
 
 	# TODO catch invalid saddle types
@@ -71,5 +76,5 @@ done
 no_actions=$(($no_actions-1))
 
 if [ $no_actions -gt 1 ]; then
-	sh run_gambit_symmetric.sh $no_actions $iterations
+	sh run_gambit_symmetric.sh $no_actions $iterations $game_type
 fi
